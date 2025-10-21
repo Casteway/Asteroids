4768bed806c2a3f038be96df6a9754efb9aec302
Krystian <napier2211@gmail.com>
2025-10-21 10:25:20 +0200

    added asteroid splitting

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collide(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         screen.fill("black")
         for sprite in drawable:
